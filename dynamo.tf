3ad663f7d4ab81aaf5e8cb6fc8786e24db8f2a0d
alberto <alberto@xanadu.ai>
2022-07-05 10:47:37 -0400

    change dynamodb capacity

--- dynamo.tf
+++ dynamo.tf
@@ -2,7 +2,9 @@
 # if you want you can increase thhe read write capacity FYI it will add costs
 resource "aws_dynamodb_table" "default" {
   name           = var.dynamo_table_name
-  billing_mode   = "PAY_PER_REQUEST"
+  billing_mode   = "PROVISIONED"
+  write_capacity = 1
+  read_capacity  = 5
   hash_key       = "slug"
   attribute {
     name = "slug"
